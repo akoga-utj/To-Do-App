2cb61a7a5ded3eeef710d14723dc7c981fbc100f
akoga-utj <akoga@utj.co.jp>
2026-04-30 14:35:45 +0900

    ブランチ切り替えのためのコミット

---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strict';
+import App from "./main.tsx";
+
+    {
+      const root = ReactDOM.createRoot(document.querySelector("#root"));
+      root.render(
+        <App />
+      );
+    }
--- src/main.tsx
+++ src/main.tsx
@@ -135,9 +135,4 @@
           </>
         );
       };
-
-      const root = ReactDOM.createRoot(document.querySelector("#root"));
-      root.render(
-        <App />
-      );
     }
